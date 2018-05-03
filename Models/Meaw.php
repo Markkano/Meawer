920de0045417e59086830493e68829db64d903de
Hrodrick <rodrigosoria.epic@gmail.com>
2018-05-02 21:46:22 -0300

    meaw model

---
+++ Models/Meaw.php
@@ -0,0 +1,56 @@
+<?php namespace Models;
+
+
+public class Meaw {
+  
+	private $idMeaw;      	//int
+	private $kitten;    	//Kitten object
+	private $image;     	//name of the image
+	private $publishDate; 	//datetime
+	private $content 		//varchar(280);
+	private $comments		//Comment object Array
+	
+	public function __construct($Kitten, $imageName, $publishDate, $content, $comments){
+		$this->kitten = $Kitten;
+		$this->image = $ImageName;
+		$this->publishDate = $publishDate;
+		$this->content = $content;
+		$this->comments = $comments;
+	}
+
+	public function getId(){
+		return $this->idMeaw;
+	}
+	public function setId($idMeaw){
+		$this->idMeaw = $idMeaw;
+	}
+	
+	public function getKitten(){
+		return $this->kitten;
+	}
+	public function setKitten($kitten){
+		$this->kitten = $kitten;
+	}
+	
+	public function getImageName(){
+		return $this->image;
+	}
+	public function setImageName($image){
+		$this->image = $image;
+	}
+	
+	public function getPublishDate(){
+		return $this->publishDate;
+	}
+	public function setPublishDate($publishDate){
+		$this->publishDate = $publishDate;
+	}
+	
+	public function getContent(){
+		return $this->content;
+	}
+	public function setContent($content){
+		$this->content = content;
+	}
+	
+} ?>
