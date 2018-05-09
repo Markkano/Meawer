7648a7b55fb23dddac91194d54d7aa7c05a2f344
Hrodrick <rodrigosoria.epic@gmail.com>
2018-05-09 20:37:13 -0300

    Aplicacion cambios locales que si no comitteo no puedo cambiar de branch GG

--- Daos/MeawDAO.php
+++ Daos/MeawDAO.php
@@ -1,8 +1,9 @@
 <?php namespace Daos;
-
+	
 	use Config\Connection as Connection;
 
-	class MeawDAO extends SingletonDao implements Idao{
+
+	class MeawDao extends SingletonDao implements Idao{
 
 		private $table; //Name of the table.
 		private $pdo;
@@ -17,13 +18,14 @@
 			//$this->commentDao = CommentDao::getInstance();
 		}
 
-		// IDAO functions.
+
+		
 		public function Insert($object){
 			$stmt = $this->pdo->prepare("insert into ".$this->table."(id_kitten, id_image, publish_date, content)"
 										." VALUES(?,?,?,?)");
 
 			//TODO : UPLOAD IMAGE AND GET ID, ASSIGN IT TO THE OBJECT, IF IT HAVES. REPLACE THE 0.
-			$params = array($object->getKitten()->getIdKitten(), 0,
+			$params = array($object->getKitten()->getIdKitten(), 0, 
 							$object->getPublishDate() ,$object->getContent());
 			$stmt->execute($params);
 			$object->setId($this->pdo->lastInsertId());
@@ -31,14 +33,19 @@
 			return $object;
 		}
 
-		public function Delete($object){ }
-		public function SelectByID($id){ }
+		public function Delete($object){
+
+		}
+
+		public function SelectByID($id){
+
+		}
 
 		public function SelectAll(){
 			$stmt = $this->pdo->prepare("select * from ".$this->table." ORDER BY id_kitten, publish_date");
 
 			$result = $stmt.fetchAll();
-
+			
 			$meaws = array();
 			$meaw;
 			$imageName  = "TestImage";
@@ -49,10 +56,16 @@
 				$meaw = new Meaw($kitten, $imageName, $row["publish_date"], $row["content"], array());
 				// TODO : get comments by meaw ID and assign them.
 				array_push($meaws, $meaw);
+
 			}
+
 			return $meaws;
 		}
 
-		public function Update($object){ }
+		public function Update($object){
 
-} ?>
+		}
+
+	}
+
+?>
